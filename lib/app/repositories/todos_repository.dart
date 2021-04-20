6e32e73455b6a37207296ca187db7d17a39b4f91
dikdudu <dikdudu@hotmail.com>
2021-04-20 14:34:17 -0300

    impremen... todo_repository

--- lib/app/repositories/todos_repository.dart
+++ lib/app/repositories/todos_repository.dart
@@ -1,1 +1,34 @@
-class TodosRepository {}
+import 'package:todo_list/app/database/connection.dart';
+import 'package:todo_list/app/models/todo_model.dart';
+
+class TodosRepository {
+  Future<List<TodoModel>> findByPeriod(DateTime start, DateTime end) async {
+    var startFilter = DateTime(start.year, start.month, start.day, 0, 0, 0);
+    var endFilter = DateTime(end.year, end.month, end.day, 0, 0, 0);
+
+    var conn = await Connection().instance;
+    var result = await conn.rawQuery(
+      "select * from todo where date_time betwenn ? and ? order by date_time",
+      [startFilter.toIso8601String(), endFilter.toIso8601String()],
+    );
+    return result.map((t) => TodoModel.fromMap(t)).toList();
+  }
+
+  Future<void> saveTodo(DateTime dateTimeTask, String description) async {
+    var conn = await Connection().instance;
+
+    await conn.rawInsert(
+      "insert into todo values(?,?,?,?)",
+      [null, description, dateTimeTask.toIso8601String(), 0],
+    );
+  }
+
+  Future<void> checkOrUncheckTodo(TodoModel todo) async {
+    var conn = await Connection().instance;
+
+    await conn.rawUpdate(
+      "update todo set finished = ? where id = ?",
+      [todo.finished ? 1 : 0, todo.id],
+    );
+  }
+}
