54f385ad96e52d4cd3f9689ac36be8809d6c0f7a
dikdudu <dikdudu@hotmail.com>
2021-04-20 14:45:33 -0300

    creating bottomNavigationBar interface with FFNavigationBar

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -3,6 +3,7 @@ import 'package:todo_list/app/repositories/todos_repository.dart';
 
 class HomeController extends ChangeNotifier {
   final TodosRepository repository;
+  int selectedTab = 1;
 
   HomeController({@required this.repository});
 }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ff_navigation_bar/ff_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/modules/home/home_controller.dart';
@@ -10,6 +11,35 @@ class HomePage extends StatelessWidget {
         return Scaffold(
           appBar: AppBar(
             title: Text('Tarefa'),
+          ),
+          bottomNavigationBar: FFNavigationBar(
+            selectedIndex: controller.selectedTab,
+            onSelectTab: (index) {},
+            items: [
+              FFNavigationBarItem(
+                iconData: Icons.check_circle,
+                label: 'Finalizados',
+              ),
+              FFNavigationBarItem(
+                iconData: Icons.view_week,
+                label: 'Semanal',
+              ),
+              FFNavigationBarItem(
+                iconData: Icons.calendar_today,
+                label: 'Selecionar Data',
+              )
+            ],
+            theme: FFNavigationBarTheme(
+              itemWidth: 60,
+              barHeight: 70,
+              barBackgroundColor: Theme.of(context).primaryColor,
+              unselectedItemIconColor: Colors.white,
+              unselectedItemLabelColor: Colors.white,
+              selectedItemBorderColor: Colors.white,
+              selectedItemIconColor: Colors.white,
+              selectedItemBackgroundColor: Theme.of(context).primaryColor,
+              selectedItemLabelColor: Colors.black,
+            ),
           ),
         );
       },
