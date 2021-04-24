d80f3855092c154f3ea9c9867803ee34814448fe
dikdudu <dikdudu@hotmail.com>
2021-04-23 23:09:27 -0300

    implements findAllForWeerk

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/foundation.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_list/app/models/todo_model.dart';
 import 'package:todo_list/app/repositories/todos_repository.dart';
+import 'package:collection/collection.dart';
 
 class HomeController extends ChangeNotifier {
   final TodosRepository repository;
   int selectedTab = 1;
+  DateTime startFilter;
+  DateTime endFilter;
 
-  HomeController({@required this.repository});
+  Map<String, List<TodoModel>> listTodos;
+
+  HomeController({@required this.repository}) {
+    findAllForWeek();
+  }
 
   void changeSelected(index) {
     selectedTab = index;
     notifyListeners();
+  }
+
+  Future<void> findAllForWeek() async {
+    var dateFormat = DateFormat('dd/MM/yyyy');
+
+    startFilter = DateTime.now();
+
+    if (startFilter.weekday != DateTime.monday) {
+      startFilter =
+          startFilter.subtract(Duration(days: (startFilter.weekday - 1)));
+    }
+
+    endFilter = startFilter.add(Duration(days: 6));
+
+    var todos = await repository.findByPeriod(startFilter, endFilter);
+
+    if (todos.isEmpty) {
+      listTodos = {dateFormat.format(DateTime.now()): []};
+    } else {
+      listTodos =
+          groupBy(todos, (TodoModel todo) => dateFormat.format(todo.dateTime));
+    }
+
+    this.notifyListeners();
   }
 }
