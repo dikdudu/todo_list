aa69e537fb751b14a26935aa1adcb7d025b924f2
dikdudu <dikdudu@hotmail.com>
2021-05-10 19:23:25 -0300

    create validator

--- lib/app/modules/new_task/new_task_controller.dart
+++ lib/app/modules/new_task/new_task_controller.dart
@@ -6,10 +6,23 @@ class NewTaskController extends ChangeNotifier {
   final TodosRepository repository;
   DateTime daySelected;
   final dateFormat = DateFormat('dd/MM/yyyy');
+  TextEditingController nameTaskController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
 
   String get dayFormated => dateFormat.format(daySelected);
 
   NewTaskController({@required this.repository, String day}) {
     daySelected = dateFormat.parse(day);
+  }
+
+  String validator(value) {
+    if (value.isEmpty) {
+      return 'Nome da Tarefa e obrigatório';
+    }
+    return null;
+  }
+
+  void save() {
+    if (formKey.currentState.validate()) {}
   }
 }
--- lib/app/modules/new_task/new_task_page.dart
+++ lib/app/modules/new_task/new_task_page.dart
@@ -18,80 +18,87 @@ class NewTaskPage extends StatelessWidget {
           elevation: 0,
         ),
         body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'NOVA TASK',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 30),
-                Text(
-                  'Data',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontWeight: FontWeight.bold,
+          child: Form(
+            key: controller.formKey,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'NOVA TASK',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
-                ),
-                Text(
-                  controller.dayFormated,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.grey[800],
-                    fontWeight: FontWeight.bold,
+                  SizedBox(height: 30),
+                  Text(
+                    'Data',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Nome da Task',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontWeight: FontWeight.bold,
+                  Text(
+                    controller.dayFormated,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 10),
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                  SizedBox(height: 20),
+                  Text(
+                    'Nome da Task',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Hora',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontWeight: FontWeight.bold,
+                  SizedBox(height: 10),
+                  TextFormField(
+                    validator: (value) => controller.validator(value),
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                SizedBox(height: 20),
-                TimeComponent(),
-                SizedBox(height: 50),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(0),
-                    boxShadow: [
-                      BoxShadow(
-                          // blurRadius: 30,
-                          // color: Theme.of(context).primaryColor,
+                  SizedBox(height: 20),
+                  Text(
+                    'Hora',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TimeComponent(),
+                  SizedBox(height: 50),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(0),
+                      boxShadow: [
+                        BoxShadow(
+                            // blurRadius: 30,
+                            // color: Theme.of(context).primaryColor,
+                            ),
+                      ],
+                    ),
+                    child: InkWell(
+                      onTap: () => controller.save(),
+                      child: Center(
+                        child: Text(
+                          'Salvar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
                           ),
-                    ],
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Salvar',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
